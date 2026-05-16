b21ba9c6e4ec5b1011f6d8b38f4f340ecbcecf49
rjs0429 <rjs0429@gmail.com>
2026-05-17 03:16:19 +0900

    Update opendbc submodule to latest avante-md-dev (commit 26e50051)
    opendbc_repo updated with avante_md driver refinements

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -26,6 +26,16 @@ LaneChangeDirection = log.LaneChangeDirection
 
 ACTUATOR_FIELDS = tuple(car.CarControl.Actuators.schema.fields.keys())
 
+LOW_SPEED_LATERAL_MAX = 30 * CV.KPH_TO_MS
+LOW_SPEED_LANE_PROB_ENTER = 0.70
+LOW_SPEED_LANE_PROB_EXIT = 0.50
+LOW_SPEED_LANE_WIDTH_MIN = 2.7
+LOW_SPEED_LANE_WIDTH_MAX = 4.2
+LOW_SPEED_LANE_CONFIRM_FRAMES = int(0.3 / DT_CTRL)
+LOW_SPEED_LANE_DROP_FRAMES = int(0.1 / DT_CTRL)
+LOW_SPEED_TURN_ANGLE = 90.0
+LOW_SPEED_BLINKER_TURN_ANGLE = 45.0
+
 
 class Controls:
   def __init__(self) -> None:
@@ -44,6 +54,9 @@ class Controls:
     self.steer_limited_by_safety = False
     self.curvature = 0.0
     self.desired_curvature = 0.0
+    self.low_speed_lat_ready = False
+    self.low_speed_lane_good_frames = 0
+    self.low_speed_lane_bad_frames = 0
 
     self.pose_calibrator = PoseCalibrator()
     self.calibrated_pose: Pose | None = None
@@ -57,6 +70,59 @@ class Controls:
       self.LaC = LatControlPID(self.CP, self.CI, DT_CTRL)
     elif self.CP.lateralTuning.which() == 'torque':
       self.LaC = LatControlTorque(self.CP, self.CI, DT_CTRL)
+
+  @staticmethod
+  def _lane_data(model_v2) -> tuple[float, float] | None:
+    if len(model_v2.laneLineProbs) <= 2 or len(model_v2.laneLines) <= 2:
+      return None
+    if not len(model_v2.laneLines[1].y) or not len(model_v2.laneLines[2].y):
+      return None
+
+    lane_prob = min(float(model_v2.laneLineProbs[1]), float(model_v2.laneLineProbs[2]))
+    lane_width = float(model_v2.laneLines[2].y[0] - model_v2.laneLines[1].y[0])
+    return lane_prob, lane_width
+
+  def _reset_low_speed_lat(self):
+    self.low_speed_lat_ready = False
+    self.low_speed_lane_good_frames = 0
+    self.low_speed_lane_bad_frames = 0
+
+  def _low_speed_lat_allowed(self, CS, model_v2) -> bool:
+    if self.CP.notCar or CS.vEgo > LOW_SPEED_LATERAL_MAX:
+      self._reset_low_speed_lat()
+      return True
+
+    turning = (abs(CS.steeringAngleDeg) > LOW_SPEED_TURN_ANGLE or
+               ((CS.leftBlinker or CS.rightBlinker) and abs(CS.steeringAngleDeg) > LOW_SPEED_BLINKER_TURN_ANGLE))
+    if turning:
+      self._reset_low_speed_lat()
+      return False
+
+    lane_data = self._lane_data(model_v2)
+    lane_confirmed = False
+    lane_lost = True
+    if lane_data is not None:
+      lane_prob, lane_width = lane_data
+      lane_width_valid = LOW_SPEED_LANE_WIDTH_MIN <= lane_width <= LOW_SPEED_LANE_WIDTH_MAX
+      lane_confirmed = lane_prob >= LOW_SPEED_LANE_PROB_ENTER and lane_width_valid
+      lane_lost = lane_prob < LOW_SPEED_LANE_PROB_EXIT or not lane_width_valid
+
+    if lane_confirmed:
+      self.low_speed_lane_good_frames += 1
+      self.low_speed_lane_bad_frames = 0
+    elif lane_lost:
+      self.low_speed_lane_good_frames = 0
+      self.low_speed_lane_bad_frames += 1
+    else:
+      self.low_speed_lane_good_frames = 0
+      self.low_speed_lane_bad_frames = 0
+
+    if self.low_speed_lane_good_frames >= LOW_SPEED_LANE_CONFIRM_FRAMES:
+      self.low_speed_lat_ready = True
+    if self.low_speed_lane_bad_frames >= LOW_SPEED_LANE_DROP_FRAMES:
+      self.low_speed_lat_ready = False
+
+    return self.low_speed_lat_ready
 
   def update(self):
     self.sm.update(15)
@@ -93,8 +159,12 @@ class Controls:
 
     # Check which actuators can be enabled
     standstill = abs(CS.vEgo) <= max(self.CP.minSteerSpeed, 0.3) or CS.standstill
-    CC.latActive = self.sm['selfdriveState'].active and not CS.steerFaultTemporary and not CS.steerFaultPermanent and \
-                   (not standstill or self.CP.steerAtStandstill)
+    low_speed_lat_allowed = self._low_speed_lat_allowed(CS, model_v2)
+    CC.latActive = (self.sm['selfdriveState'].active and
+                    not CS.steerFaultTemporary and
+                    not CS.steerFaultPermanent and
+                    (not standstill or self.CP.steerAtStandstill) and
+                    low_speed_lat_allowed)
     CC.longActive = CC.enabled and not any(e.overrideLongitudinal for e in self.sm['onroadEvents']) and self.CP.openpilotLongitudinalControl
 
     actuators = CC.actuators
